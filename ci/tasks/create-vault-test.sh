bacc2ef6f35f37b6069b97b10bda89b2b9934ccd
Steve Wall <swall@ecsteam.com>
2017-09-18 17:10:09 -0600

    ci pipeline build out

--- ci/tasks/create-vault-test.sh
+++ ci/tasks/create-vault-test.sh
@@ -16,6 +16,8 @@ export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=$BOSH_ADMIN_PASSWORD
 export BOSH_ENVIRONMENT="concourse-director"
 
-bosh2 alias-env $BOSH_ENVIRONMENT -e $BOSH_DIRECTOR --ca-cert $BOSH_CA
+cat $BOSH_CA > boshca.pem
+
+bosh2 alias-env $BOSH_ENVIRONMENT -e $BOSH_DIRECTOR --ca-cert boshca.pem
 
 bosh2 is
