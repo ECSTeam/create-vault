3cd185fec5e7f3b20b0c0d598bd355679b7e6ec2
Steve Wall <swall@ecsteam.com>
2017-09-19 08:49:11 -0600

    Trying to get pem in the right format

--- ci/tasks/create-vault-test.sh
+++ ci/tasks/create-vault-test.sh
@@ -16,8 +16,12 @@ export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=$BOSH_ADMIN_PASSWORD
 export BOSH_ENVIRONMENT="concourse-director"
 
+# The cert comes in as a string. Need to convert it back to a proper PEM format
 echo $BOSH_CA > boshca.pem
+tr ' ' '\n' boshca.pem > newline.pem
+sed -e ':a' -e 'N' -e '$!ba' -e 's/N\nC/N C/g' newline.pem > topfixed.pem
+sed -e ':a' -e 'N' -e '$!ba' -e 's/D\nC/D C/g' topfixed.pem > finalboshca.pem
 
-bosh2 alias-env $BOSH_ENVIRONMENT -e $BOSH_DIRECTOR --ca-cert boshca.pem
+bosh2 alias-env $BOSH_ENVIRONMENT -e $BOSH_DIRECTOR --ca-cert finalboshca.pem
 
 bosh2 is
